3a3d1e91e58cb5462302b5dbd9b529f90560353d
Sabrina Yohana <s.yohana@escolar.ifrn.edu.br>
2025-05-14 10:55:49 -0300

    l4

---
+++ L04/q03.py
@@ -0,0 +1,18 @@
+a = int (input())
+b = int (input())
+c = int (input())
+d = int (input())
+e = int (input())
+
+
+maior = a
+if (b>=maior):
+  maior = b
+if (c>=maior):
+  maior = c
+if (d>=maior):
+  maior = d
+if (e>=maior):
+  maior = e
+  
+print(maior)
